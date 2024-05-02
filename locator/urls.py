26aca8a81a8ee6c29f6f577fc3b2c32acac2bb15
Billy Ashmall <billy.ashmall@nasa.gov>
2024-05-02 14:06:40 -0500

    path

--- locator/urls.py
+++ locator/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 import locator.views as views
 
 urlpatterns = [
-    path('/get_country_code/', views.get_country_code, name='get_country_code'),
+    path('get_country_code/', views.get_country_code, name='get_country_code'),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
