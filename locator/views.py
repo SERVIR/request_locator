aa079ffc119b8c89123f023d6e84da29a6deb9ce
Billy Ashmall <billy.ashmall@nasa.gov>
2024-05-03 10:49:26 -0500

    cleanup

--- locator/views.py
+++ locator/views.py
@@ -4,11 +4,11 @@ from django.contrib.gis.geoip2 import GeoIP2
 from django.views.decorators.csrf import csrf_exempt
 from geoip2.errors import AddressNotFoundError
 
-# Create your views here.
-
+# Initialize GeoIP2 instance
 g = GeoIP2()
 
 
+# Function to get client IP address
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
@@ -18,6 +18,7 @@ def get_client_ip(request):
     return ip
 
 
+# Endpoint to get country code
 @csrf_exempt
 def get_country_code(request):
     ip_address = request.GET.get('ip_address')
@@ -29,9 +30,11 @@ def get_country_code(request):
 
         return JsonResponse({"country_code": country_code})
     except AddressNotFoundError:
+        # If address is not found, return ZZ as the country code
         return JsonResponse({"country_code": "ZZ"})
-    
-    
+
+
+# Endpoint to get country
 @csrf_exempt
 def get_country(request):
     ip_address = request.GET.get('ip_address')
@@ -43,9 +46,11 @@ def get_country(request):
 
         return JsonResponse({"country": country})
     except AddressNotFoundError:
+        # If address is not found, return None for country code and country name
         return JsonResponse({"country": {"country_code": None, "country_name": None}})
 
 
+# Endpoint to get city
 @csrf_exempt
 def get_city(request):
     ip_address = request.GET.get('ip_address')
@@ -56,4 +61,8 @@ def get_city(request):
             city = g.city(get_client_ip(request))
         return JsonResponse({"city": city})
     except AddressNotFoundError:
-        return JsonResponse({"city": {"city": None, "continent_code": None, "continent_name": None, "country_code": None, "country_name": None, "dma_code": None, "is_in_european_union": None, "latitude": None, "longitude": None, "postal_code": None, "region": None, "time_zone": None}})
+        # If address is not found, return None for all city details
+        return JsonResponse({"city": {"city": None, "continent_code": None, "continent_name": None,
+                                      "country_code": None, "country_name": None, "dma_code": None,
+                                      "is_in_european_union": None, "latitude": None, "longitude": None,
+                                      "postal_code": None, "region": None, "time_zone": None}})
--- request_locator/settings.py
+++ request_locator/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 f = open(str(BASE_DIR) + '/data.json', )
 data = json.load(f)
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -32,7 +31,6 @@ DEBUG = True
 ALLOWED_HOSTS = data['ALLOWED_HOSTS']
 
 CSRF_TRUSTED_ORIGINS = data['CSRF_TRUSTED_ORIGINS']
-
 
 # Application definition
 
@@ -78,7 +76,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "request_locator.wsgi.application"
 GEOIP_PATH = data["GEOIP_PATH"]
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -88,7 +85,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -108,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -119,7 +114,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
